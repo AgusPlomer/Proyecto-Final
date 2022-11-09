9cc0f3c5512b3bc0842680380a5e979b5bec2f42
agustin plomer <agus_plomer@hotmail.com.ar>
2022-11-09 07:31:01 -0300

    arreglar formulario (no devuelve datos)

---
+++ html/formulario.js
@@ -0,0 +1,30 @@
+function guardardatos(){
+let nombre=document.getElementById('nombre');
+let apellido=document.getElementById('apellido');
+let correo=document.getElementById('correo');
+ let button=document.getElementById('boton');
+
+let datos=JSON.parse(localStorage.setItem('datosform'));
+
+if(datos== null){
+localStorage.setItem('datosform','[]');
+datos=[];
+};
+
+let auxregistro={
+    Nombre:nombre.value,
+    Apellido:apellido.value,
+    Correo:correo.value
+}
+
+datos.push(auxregistro);
+
+localStorage.setItem('datosform',JSON.stringify(datos));
+
+console.log('fdsf')
+ button.addEventListener('click',guardardatos);
+
+};
+
+// let button=document.getElementById('boton');
+// button.addEventListener('click',guardardatos);
